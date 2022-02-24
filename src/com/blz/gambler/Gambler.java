43d57fc0c310e089f068ad5ebf01676a0cda82c8
rohgr <rdagrawal6@gmail.com>
2022-02-24 15:44:15 +0530

    Addin UC5 to Gamble simulator

--- src/com/blz/gambler/Gambler.java
+++ src/com/blz/gambler/Gambler.java
@@ -1,13 +1,14 @@
 package com.blz.gambler;
-import static java.lang.System.exit;
 public class Gambler {
     public static final int GAMBLER_WITH_MONEY = 100;
     public static final int GAMBLER_BET = 1;
-
+    public static final int MONTH_DAYS = 30;
+    public static final int Days = 1;
     public static void main(String[] args) {
            int total_Amount = 0 ;
+           int i = Days;
         System.out.println("gambler money: " + GAMBLER_WITH_MONEY);
-        for (int i = 1; i <= 20; i++) {
+        for ( i = 1; i <= MONTH_DAYS; i++) {
             int total_Cash = GAMBLER_WITH_MONEY;
             int bet = GAMBLER_BET;
             while (total_Cash < 150 && total_Cash > 50) {
@@ -20,8 +21,11 @@ public class Gambler {
             }
             if (total_Cash == 150) {
                 total_Amount +=50;
+                System.out.println( "Gambler won " +total_Amount+ " on Day " +i);
             } else {
                 total_Amount -=50;
+                System.out.println( "Gambler loss " +total_Amount+ " on Day " +i);
+
             }
         }
             if(total_Amount > 0){
