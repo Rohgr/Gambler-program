371cdc1be0604caa90b4ec81da06655e379801e1
rohgr <rdagrawal6@gmail.com>
2022-02-24 08:21:15 +0530

    Adding Gambler Program

---
+++ src/Gambler.java
@@ -0,0 +1,12 @@
+
+
+
+public class Gambler {
+    public static void main(String[] args) {
+    int gambler_With_Money= 100; //Starting money everyday 100
+    int gambler_Per_Bet= 1 ;//money per bet = 1
+
+        System.out.println("Welcome to Gambler game");
+    }
+
+}
